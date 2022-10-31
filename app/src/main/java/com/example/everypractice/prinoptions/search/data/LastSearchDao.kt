57caaf2cb0c48289652bb8f00c4b37aca05bbad3
Alvaro Ramirez <ralvaro.dev@gmail.com>
2022-10-31 18:59:55 -0500

    rename dao functions

--- app/src/main/java/com/example/everypractice/prinoptions/search/data/LastSearchDao.kt
+++ app/src/main/java/com/example/everypractice/prinoptions/search/data/LastSearchDao.kt
@@ -2,27 +2,26 @@ package com.example.everypractice.prinoptions.search.data
 
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 @Dao
 interface LastSearchDao {
 
     @Query("SELECT * from historial ORDER BY id DESC LIMIT 10")
-    fun getHistorial(): Flow<List<LastSearch>>
+    fun getLastSearchList(): Flow<List<LastSearch>>
 
     @Query("SELECT last_search from historial")
-    fun getListWordsHistorial(): List<String>
+    fun getWordsOfLastSearchList(): List<String>
 
     @Query("SELECT * from historial WHERE id = :id")
-    fun getOneHistorial(id: Int): Flow<LastSearch>
+    fun getLastSearchById(id: Int): Flow<LastSearch>
 
     @Query("SELECT id from historial WHERE last_search = :lastSearch")
-    fun getIdAlreadySearch(lastSearch: String): Int
+    fun getIdByWord(lastSearch: String): Int
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(historial : LastSearch)
+    suspend fun insert(historial: LastSearch)
 
     @Delete
-    suspend fun delete(historial : LastSearch)
+    suspend fun delete(historial: LastSearch)
 
 }
--- app/src/main/java/com/example/everypractice/prinoptions/search/repository/SearchRepository.kt
+++ app/src/main/java/com/example/everypractice/prinoptions/search/repository/SearchRepository.kt
@@ -1,28 +1,24 @@
 package com.example.everypractice.prinoptions.search.repository
 
 import androidx.annotation.WorkerThread
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import com.example.everypractice.prinoptions.search.data.LastSearch
 import com.example.everypractice.prinoptions.search.data.LastSearchDao
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
 
 //ESTE NOS DA EL ACCESO A LAS FUNCIONES DE MODIFICACION DE LA BASE DE DATOS, SINE XPONER TODO LA BASE DDE DATOS
 class SearchRepository(private val lastSearchDao: LastSearchDao) {
 
-    val allHistory: Flow<List<LastSearch>> = lastSearchDao.getHistorial()
+    val allHistory: Flow<List<LastSearch>> = lastSearchDao.getLastSearchList()
 
     fun obtainOneHistorial(id: Int): Flow<LastSearch> {
-        return lastSearchDao.getOneHistorial(id)
+        return lastSearchDao.getLastSearchById(id)
     }
 
     fun obtainIdAlreadySearch(lastSearch: String): Int {
-        return lastSearchDao.getIdAlreadySearch(lastSearch)
+        return lastSearchDao.getIdByWord(lastSearch)
     }
 
-    fun obtainListWords() = lastSearchDao.getListWordsHistorial()
+    fun obtainListWords() = lastSearchDao.getWordsOfLastSearchList()
 
     @WorkerThread
     suspend fun insert(lastSearch: LastSearch) {
